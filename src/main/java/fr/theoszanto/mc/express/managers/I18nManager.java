bb893af97acd48c7ab3aa675f4348b9c28b60595
indyteo <theo.mail@laposte.net>
2025-06-14 00:40:53 +0200

    Improved i18n number formatting with optional plural mark

--- src/main/java/fr/theoszanto/mc/express/managers/I18nManager.java
+++ src/main/java/fr/theoszanto/mc/express/managers/I18nManager.java
@@ -53,6 +53,8 @@ public class I18nManager<P extends ExpressPlugin<P>> extends Registry<P, String,
 			if (!(key instanceof String))
 				throw new IllegalArgumentException("Illegal format parameter key (must be String): " + key);
 			message = message.replaceAll("#\\{" + key + "}", Matcher.quoteReplacement(String.valueOf(value)));
+			if (value instanceof Number number)
+				message = message.replaceAll("#\\{" + key + ":s}", number.doubleValue() < 2 ? "" : "s");
 		}
 		return message;
 	}
